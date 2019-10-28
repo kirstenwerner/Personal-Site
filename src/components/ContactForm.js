d5966c7221d1793de4ba5fb6a7817630e56490e9
kirstenwerner <kirstenmwerner@gmail.com>
2019-10-28 11:35:15 -0700

    fixed toasts to display on outside layer of components

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,165 +1,176 @@
-import React from 'react';
-import * as emailjs from 'emailjs-com';
-import 'jquery'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import * as emailjs from "emailjs-com";
+import "jquery";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 let container;
+const EmailJSID = process.env.REACT_APP_EMAILJS_USERID;
+const Email_address = process.env.TO_NAME;
 
 export default class extends React.Component {
   constructor(props) {
-  	super(props)
+    super(props);
 
     this.state = {
-      name: '',
-      email: '',
-      feedback: '',
+      name: "",
+      email: "",
+      feedback: "",
       errors: {
-        name: '',
-        email_1: '',
-        email_2: '',
-        feedback: '',
+        name: "",
+        email_1: "",
+        email_2: "",
+        feedback: ""
       }
-    }
+    };
   }
 
-  handleInputChange (event) {
-    event.preventDefault()
-    const target = event.target
-    const name = target.name
-    const value = target.value
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
 
-    this.setState({[name]: value})
+    this.setState({ [name]: value });
   }
 
-  validateMail () {
-    let errors = {}
-    let formIsValid = true
+  validateMail() {
+    let errors = {};
+    let formIsValid = true;
 
     if (!this.state.name || this.state.name.length < 3) {
-      errors.name = "I'd love it if your name was at least 3 characters long..."
+      errors.name =
+        "I'd love it if your name was at least 3 characters long...";
       toast.error(`${errors.name}`, {
-        position: toast.POSITION.TOP_LEFT      });
-      formIsValid = false
+        position: toast.POSITION.TOP_LEFT
+      });
+      formIsValid = false;
     }
 
     if (!this.state.feedback || this.state.feedback.length < 10) {
-      errors.feedback = `C'mon, the body of your message should say SOMEthing!`
+      errors.feedback = `C'mon, the body of your message should say SOMEthing!`;
       toast.error(`${errors.feedback}`);
-      formIsValid = false
+      formIsValid = false;
     }
 
     if (!this.state.email || this.state.email.length < 3) {
-      errors.email_1 = 'Your email address has got to have at least 3 characters.'
+      errors.email_1 =
+        "Your email address has got to have at least 3 characters.";
       toast.error(`${errors.email_1}`);
-      formIsValid = false
+      formIsValid = false;
     }
 
-    let pattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    let pattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
     if (!pattern.test(this.state.email)) {
-      errors.email_2 = 'Email addresses need an @ and a .com'
+      errors.email_2 = "Email addresses need an @ and a .com";
       toast.error(`${errors.email_2}`);
-      formIsValid = false
+      formIsValid = false;
     }
 
     this.setState({
       errors: errors
-    })
+    });
 
-    console.log(errors)
-    return formIsValid
+    console.log(errors);
+    return formIsValid;
   }
 
-  sendMessage (event) {
-    event.preventDefault()
+  sendMessage(event) {
+    event.preventDefault();
 
     if (!this.validateMail()) {
-      return
+      return;
     }
 
     const templateParams = {
-      from_name: this.state.name + '(' + this.state.email + ')',
-      to_name: 'kirstenmwerner@gmail.com',
+      from_name: this.state.name + " (" + this.state.email + ")",
+      to_name: `${Email_address}`,
       feedback: this.state.feedback
-    }
+    };
 
-    emailjs.send('gmail', 'portfoliositecontact', templateParams, 'user_ZQYRt26t8ataC1aDEHvXx')
-      .then(function (response) {
-        toast.success("Your message has successfully sent!", {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-        console.log('SUCCESS!', response.status, response.text)
-      }, function (err) {
-        toast.error('Your message was not able to be sent')
-      })
+    emailjs
+      .send("gmail", "portfoliositecontact", templateParams, `${EmailJSID}`)
+      .then(
+        function(response) {
+          toast.success("Your message has successfully sent!", {
+            position: toast.POSITION.BOTTOM_CENTER
+          });
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function(err) {
+          toast.error("Your message was not able to be sent");
+        }
+      );
 
     this.setState({
-      name: '',
-      email: '',
-      feedback: ''
-    })
+      name: "",
+      email: "",
+      feedback: ""
+    });
   }
 
   render() {
-	return (
-    <div>
-  	<form
-      className="ui form"
-      id={this.props.id}
-      name={this.props.name}
-      method={this.props.method}
-      action={this.props.action}
-    >
-        <textarea
-          id="name"
-          name="name"
-          onChange={this.handleInputChange.bind(this)}
-          placeholder="your name"
-          required
-          value={this.state.name}
-          error={this.state.errors.name}
-          style={{width: '100%'}}
-          rows={1}
-        />
-      <br />
-      <br />
-        <textarea
-        	id="email"
-        	name="email"
-        	onChange={this.handleInputChange.bind(this)}
-        	placeholder="your email address"
-        	required
-        	value={this.state.email}
-          error={this.state.errors.email}
-        	style={{width: '100%'}}
-          rows={1}
-      	/>
-        <br />
-        <br />
-      	<textarea
-        	id="feedback"
-        	name="feedback"
-        	onChange={this.handleInputChange.bind(this)}
-        	placeholder="what would you like to chat about?"
-        	required
-        	value={this.state.feedback}
-          error={this.state.errors.feedback}
-        	style={{width: '100%', height: '250px'}}
-      	/>
-      <br />
-      <br />
-    	<input
-        type="button"
-        value="Send"
-        className="ui button"
-        style={{
-          fontFamily: 'Amatic SC',
-          fontSize: '20px',
-          color: 'blue'
-        }}
-        onClick={this.sendMessage.bind(this)} />
-  	</form>
-    </div>
-	)}
+    return (
+      <div>
+        <form
+          className="ui form"
+          id={this.props.id}
+          name={this.props.name}
+          method={this.props.method}
+          action={this.props.action}
+        >
+          <textarea
+            id="name"
+            name="name"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="your name"
+            required
+            value={this.state.name}
+            error={this.state.errors.name}
+            style={{ width: "100%" }}
+            rows={1}
+          />
+          <br />
+          <br />
+          <textarea
+            id="email"
+            name="email"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="your email address"
+            required
+            value={this.state.email}
+            error={this.state.errors.email}
+            style={{ width: "100%" }}
+            rows={1}
+          />
+          <br />
+          <br />
+          <textarea
+            id="feedback"
+            name="feedback"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="what would you like to chat about?"
+            required
+            value={this.state.feedback}
+            error={this.state.errors.feedback}
+            style={{ width: "100%", height: "250px" }}
+          />
+          <br />
+          <br />
+          <input
+            type="button"
+            value="Send"
+            className="ui button"
+            style={{
+              fontFamily: "Amatic SC",
+              fontSize: "20px",
+              color: "blue"
+            }}
+            onClick={this.sendMessage.bind(this)}
+          />
+        </form>
+      </div>
+    );
+  }
 }
